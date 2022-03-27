8d416d79af11788fc3f828ee9ee411c6cfefcf03
Candice Hall <hall.candice@outlook.com>
2022-03-27 16:45:15 -0400

    added list of current weather details

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,8 @@
 var locationInput = document.querySelector("#city");
 var userForm = document.querySelector("#user-form");
 var currentWeather = document.querySelector("#current-weather");
+var listEl = document.querySelector(".details");
+var currentWContainer = document.querySelector(".current-weather-container");
 // var submitBtn = document.querySelector(".get-btn");
 
 var formSubmitHandler = function (event) {
@@ -40,12 +42,13 @@ var getApiLocation = function (location) {
 }
 
 var getWeather = function (lat, long) {
-    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=9c6774c19b3c951137a1b16a4660a14e";
+    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=metric&appid=9c6774c19b3c951137a1b16a4660a14e";
     fetch(apiURL).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                // console.log(data);
-            displayWeather(data);
+                console.log(data);
+                displayWeather(data);
+
             })
         } else {
             alert("Opps! something went wrong. Try again!");
@@ -53,17 +56,46 @@ var getWeather = function (lat, long) {
     })
 }
 
-var displayLocation = function(location){
-
+var displayLocation = function (location) {
+    currentWeather.innerHTML = "";
+    listEl.innerHTML = "";
+    currentWContainer.setAttribute("class", "border")
     var date = moment().format('MMMM Do YYYY')
     var weatherH2 = document.createElement("h2");
     weatherH2.textContent = location + " (" + date + ")";
-    currentWeather.setAttribute("class", "border");
+    currentWeather.setAttribute("class", "text-uppercase");
     currentWeather.appendChild(weatherH2);
 }
 
-var displayWeather = function(data){
+var displayWeather = function (data) {
+    var temp = document.createElement("li");
+    temp.textContent = "Temp: " + Math.round(data.current.temp) + "°C (Feels like " + Math.round(data.current.feels_like) + "°C)";
+    listEl.appendChild(temp);
 
+    var wind = document.createElement("li");
+    //dont forget to fix this!!!!! not current mph, but mps
+    wind.textContent = "Wind: " + data.current.wind_speed + " MPH";
+    listEl.appendChild(wind);
+
+    var humidity = document.createElement("li");
+    humidity.textContent = "Humidity: " + data.current.humidity + "%";
+    listEl.appendChild(humidity);
+
+    var uvIndex = document.createElement("li");
+    if (data.current.uvi <= 2) {
+        uvIndex.innerHTML = "UV Index: <span class='favourable'>" + data.current.uvi + "</span>";
+        listEl.appendChild(uvIndex);
+    }
+    else if (data.current.uvi > 2.1 && data.current.uvi <= 5) {
+        uvIndex.innerHTML = "UV Index: <span class='moderate'>" + data.current.uvi + "</span>";
+        listEl.appendChild(uvIndex);
+    } else {
+        uvIndex.innerHTML = "UV Index: <span class='severe'>" + data.current.uvi + "</span>";
+        listEl.appendChild(uvIndex);
+    }
 }
+
+
+
 
 userForm.addEventListener("submit", formSubmitHandler);
